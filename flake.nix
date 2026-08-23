1e86c022a319863eedc0b59bea8433f3d7a4aa62
0x0c12 <kanrojishinichi@protonmail.com>
2026-08-24 00:18:23 +0530

    wowzers

--- flake.nix
+++ flake.nix
@@ -39,5 +39,14 @@
       };
     };
 
+      homeConfigurations.twilight = 
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+	    ./nixos/home.nix
+	  ];
+        };
+
     };
 }
